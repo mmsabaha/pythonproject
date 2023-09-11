f39301be5a7e2507b1d90d0b5cd8c1ef38c2c55e
Msabaha <mmsabaha@deloitte.com>
2023-09-11 00:06:44 -0700

    initial

---
+++ main.py
@@ -0,0 +1,22 @@
+
+
+print("hello mrisho")
+
+
+def calculate(a,b): 
+    c=a+b
+    return c
+
+print(calculate(3,5))
+
+def print_food(food):
+    for f in food:
+        print(f)
+
+
+
+fruits = ["mangoes", "banana", "grapes"]
+
+print_food(fruits)
+
+
